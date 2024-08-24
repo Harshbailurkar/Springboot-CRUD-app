29a2bc07e3dca154bff92243e9e74f7af426928b
Harsh Bailurkar <harshbailurkar@gmail.com>
2024-08-24 14:14:02 +0530

    Added mysql as a database.

--- src/main/java/com/harsh/ecommers/Controller/ProductController.java
+++ src/main/java/com/harsh/ecommers/Controller/ProductController.java
@@ -3,6 +3,7 @@ package com.harsh.ecommers.Controller;
 import com.harsh.ecommers.Service.ProductService;
 import com.harsh.ecommers.Model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,21 +17,27 @@ public class ProductController {
     public List<Product> getAllProducts() {
         return service.getProducts();
     }
+
     @GetMapping("/product/{pid}")
     public Product getProduct(@PathVariable int pid) {
         return service.getProductById(pid);
     }
+
     @PostMapping("/product/add")
-    public void addProduct(@RequestBody Product product) {
+    public ResponseEntity<String> addProduct(@RequestBody Product product) {
         service.addProduct(product);
+        return ResponseEntity.ok("Product added successfully");
     }
+
     @PutMapping("/product/{pid}/update")
-    public void updateProduct(@PathVariable int pid, @RequestBody Product prod) {
-        service.updateProduct(pid,prod);
+    public ResponseEntity<String> updateProduct(@PathVariable int pid, @RequestBody Product prod) {
+        service.updateProduct(pid, prod);
+        return ResponseEntity.ok("Product updated successfully");
     }
     @DeleteMapping("/remove/{pid}")
-    public void deleteProduct(@PathVariable int pid) {
+    public ResponseEntity<String> deleteProduct(@PathVariable int pid) {
         service.deleteProduct(pid);
+        return ResponseEntity.ok("Product removed successfully");
     }
 
 }
